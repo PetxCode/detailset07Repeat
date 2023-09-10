6ebca53b9c1e154cef81d4403f7dc38ed1c30412
PetxCode <brighterdayscodelab@gmail.com>
2023-09-10 23:14:05 +0100

    start

--- src/utils/roles.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.role = void 0;
-var role;
-(function (role) {
-    role["ADMIN"] = "admin";
-    role["DISPATCHER"] = "dispatcher";
-    role["USER"] = "user";
-})(role || (exports.role = role = {}));
